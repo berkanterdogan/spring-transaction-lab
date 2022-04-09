75e07c11e46991632fea3352049700e744b83df8
berdogan <berkanterdogan92@gmail.com>
2022-04-09 22:31:01 +0300

    first commit. The project was initialized.

---
+++ src/main/java/com/berkanterdogan/lab/springtransactionlab/SpringTransactionLabApplication.java
@@ -0,0 +1,13 @@
+package com.berkanterdogan.lab.springtransactionlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTransactionLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTransactionLabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/berkanterdogan/lab/springtransactionlab/SpringTransactionLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.berkanterdogan.lab.springtransactionlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTransactionLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
